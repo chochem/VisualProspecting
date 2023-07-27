966d4ffdce3ff07d4211a9968dd56681e0bdcb37
glowredman <35727266+glowredman@users.noreply.github.com>
2023-07-27 21:40:42 +0200

    Update buildscript

--- src/main/java/com/sinthoras/visualprospecting/Tags.java
+++ src/main/java/com/sinthoras/visualprospecting/Tags.java
@@ -5,10 +5,10 @@ package com.sinthoras.visualprospecting;
 public class Tags {
 
     // GRADLETOKEN_* will be replaced by your configuration values at build time
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String MODNAME = "GRADLETOKEN_MODNAME";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
+    public static final String MODID = "visualprospecting";
+    public static final String MODNAME = "VisualProspecting";
+    public static final String VERSION = Version.VERSION;
+    public static final String GROUPNAME = "com.sinthoras.visualprospecting";
 
     public static final String VISUALPROSPECTING_DIR = MODID + "/";
     public static final String CLIENT_DIR = VISUALPROSPECTING_DIR + "client/";
