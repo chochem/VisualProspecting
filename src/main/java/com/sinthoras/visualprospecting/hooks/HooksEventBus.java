388ed14d9ae91ab91799b1425a43abc8f1f67058
glowredman <35727266+glowredman@users.noreply.github.com>
2023-07-27 21:41:56 +0200

    Use PlayerLoggedInEvent instead of EntityJoinWorldEvent

--- src/main/java/com/sinthoras/visualprospecting/hooks/HooksEventBus.java
+++ src/main/java/com/sinthoras/visualprospecting/hooks/HooksEventBus.java
@@ -1,16 +1,10 @@
 package com.sinthoras.visualprospecting.hooks;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.world.WorldEvent;
 
 import com.sinthoras.visualprospecting.Utils;
-import com.sinthoras.visualprospecting.VP;
 import com.sinthoras.visualprospecting.database.ClientCache;
 import com.sinthoras.visualprospecting.database.ServerCache;
-import com.sinthoras.visualprospecting.database.WorldIdHandler;
-import com.sinthoras.visualprospecting.network.WorldIdNotification;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -26,16 +20,5 @@ public class HooksEventBus {
     @SubscribeEvent
     public void onEvent(WorldEvent.Save event) {
         ServerCache.instance.saveVeinCache();
-    }
-
-    @SubscribeEvent
-    public void onEvent(EntityJoinWorldEvent event) {
-        if (event.world.isRemote == false) {
-            if (event.entity instanceof EntityPlayerMP) {
-                VP.network.sendTo(new WorldIdNotification(WorldIdHandler.getWorldId()), (EntityPlayerMP) event.entity);
-            } else if (event.entity instanceof EntityPlayer) {
-                ClientCache.instance.loadVeinCache(WorldIdHandler.getWorldId());
-            }
-        }
     }
 }
--- src/main/java/com/sinthoras/visualprospecting/hooks/HooksFML.java
+++ src/main/java/com/sinthoras/visualprospecting/hooks/HooksFML.java
@@ -1,9 +1,17 @@
 package com.sinthoras.visualprospecting.hooks;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+import com.sinthoras.visualprospecting.VP;
 import com.sinthoras.visualprospecting.database.ClientCache;
+import com.sinthoras.visualprospecting.database.WorldIdHandler;
+import com.sinthoras.visualprospecting.network.WorldIdNotification;
 import com.sinthoras.visualprospecting.task.TaskManager;
 
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
 
@@ -17,5 +25,14 @@ public class HooksFML {
     @SubscribeEvent
     public void onEvent(TickEvent event) {
         TaskManager.instance.onTick();
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onEvent(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.player instanceof EntityPlayerMP playerMP) {
+            VP.network.sendTo(new WorldIdNotification(WorldIdHandler.getWorldId()), playerMP);
+        } else if (event.player instanceof EntityPlayer) {
+            ClientCache.instance.loadVeinCache(WorldIdHandler.getWorldId());
+        }
     }
 }
